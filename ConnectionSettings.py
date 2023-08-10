9b6fd264108d416568b3b9dbb1b2609293e1e41a
Swagat <swagatswaroop@gmail.com>
2023-08-10 16:41:24 +0530

    Added Some Functions

--- ConnectionSettings.py
+++ ConnectionSettings.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import mysql.connector as mysql
+import pandas as pd
 from mysql.connector import Error
 
 
@@ -18,8 +19,6 @@ def create_database(query):
     except Error as err:
         print(f"Error: '{err}'")
 
-print(connection)
-
 def execute_query(connection, query):
     cursor = connection.cursor()
     try:
@@ -29,4 +28,88 @@ def execute_query(connection, query):
     except Error as err:
         print(f"Error: '{err}'")
         
+def read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print(f"Error: '{err}'")
+       
+create_teacher_table = """
+CREATE TABLE teacher (
+  teacher_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  language_1 VARCHAR(3) NOT NULL,
+  language_2 VARCHAR(3),
+  dob DATE,
+  tax_id INT UNIQUE,
+  phone_no VARCHAR(20)
+  );
+ """
+ 
+alter_participant = """
+ALTER TABLE participant
+ADD FOREIGN KEY(client)
+REFERENCES client(client_id)
+ON DELETE SET NULL;
+"""
 
+alter_course = """
+ALTER TABLE course
+ADD FOREIGN KEY(teacher)
+REFERENCES teacher(teacher_id)
+ON DELETE SET NULL;
+"""
+
+alter_course_again = """
+ALTER TABLE course
+ADD FOREIGN KEY(client)
+REFERENCES client(client_id)
+ON DELETE SET NULL;
+"""
+
+create_takescourse_table = """
+CREATE TABLE takes_course (
+  participant_id INT,
+  course_id INT,
+  PRIMARY KEY(participant_id, course_id),
+  FOREIGN KEY(participant_id) REFERENCES participant(participant_id) ON DELETE CASCADE,
+  FOREIGN KEY(course_id) REFERENCES course(course_id) ON DELETE CASCADE
+);
+"""
+ 
+pop_teacher = """
+INSERT INTO teacher VALUES
+(1,  'James', 'Smith', 'ENG', NULL, '1985-04-20', 12345, '+491774553676'),
+(2, 'Stefanie',  'Martin',  'FRA', NULL,  '1970-02-17', 23456, '+491234567890'), 
+(3, 'Steve', 'Wang',  'MAN', 'ENG', '1990-11-12', 34567, '+447840921333'),
+(4, 'Friederike',  'Müller-Rossi', 'DEU', 'ITA', '1987-07-07',  45678, '+492345678901'),
+(5, 'Isobel', 'Ivanova', 'RUS', 'ENG', '1963-05-30',  56789, '+491772635467'),
+(6, 'Niamh', 'Murphy', 'ENG', 'IRI', '1995-09-08',  67890, '+491231231232');
+"""
+
+update = """
+UPDATE client 
+SET address = '23 Fingiertweg, 14534 Berlin' 
+WHERE client_id = 101;
+"""
+
+delete_course = """
+DELETE FROM course 
+WHERE course_id = 20;
+"""
+# execute_query(connection,create_teacher_table)
+# execute_query(connection,pop_teacher)
+results = read_query(connection, "SELECT * FROM teacher")
+
+from_db = []
+for result in results:
+  result = list(result)
+  from_db.append(result)
+columns = ["teacher_id", "first_name", "last_name", "language_1", "language_2","dob", "tax_id", "phone_no"]
+df = pd.DataFrame(from_db, columns=columns)
+print(df)
